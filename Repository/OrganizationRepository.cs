107944104faf38298c3849ed7f171c42ee3b3514
Daniel Ramirez <juanfantasma1@yahoo.com>
2021-04-07 21:31:05 -0400

    Added files for module 6

---
+++ Repository/OrganizationRepository.cs
@@ -0,0 +1,15 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+
+namespace Repository
+{
+    public class OrganizationRepository : RepositoryBase<Organization>, IOrganizationRepository
+    {
+        public OrganizationRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+
+        }
+    }
+}
--- SchoolAPI/Extensions/ServiceExtensions.cs
+++ SchoolAPI/Extensions/ServiceExtensions.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Contracts;
+﻿using Contracts;
 using LoggerService;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Repository;
+using Entities;
 
 namespace SchoolAPI.Extensions
 {
@@ -14,6 +15,13 @@ namespace SchoolAPI.Extensions
 
         public static void ConfigureLoggerService(this IServiceCollection services) =>
             services.AddScoped<ILoggingManager, LoggerManager>();
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+    services.AddDbContext<RepositoryContext>(opts =>
+        opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"), b => b.MigrationsAssembly("SchoolAPI")));
+
+        public static void ConfigureRepositoryManager(this IServiceCollection services) =>
+           services.AddScoped<IRepositoryManager, RepositoryManager>();
 
     }
 }
